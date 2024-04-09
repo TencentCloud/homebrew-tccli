4b3f3046fd7a28ba75aef18afb9fececfadbbd87
HS-Gray <1978489546@qq.com>
2024-04-09 15:33:24 +0800

    fix #4

--- tccli.rb
+++ tccli.rb
@@ -12,7 +12,7 @@ class Tccli < Formula
   depends_on "python@3.9"
 
   def install
-    venv = virtualenv_create(libexec, "python3")
+    venv = virtualenv_create(libexec, "python3", without_pip: false)
     system libexec/"bin/pip", "install", "-v", 
                               "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "tccli"
