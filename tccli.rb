ac07179d84e3f0f20b4aa3e78ac5471b0470ac99
邹文杰 <72617766+19916302128@users.noreply.github.com>
2025-03-13 16:24:58 +0800

    Create tccli.rb

---
+++ tccli.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Tccli < Formula
+  include Language::Python::Virtualenv
+  desc "Tencent Cloud API 3.0 Command Line Interface"
+  homepage "https://cloud.tencent.com/document/product/440/6176"
+  url "https://github.com/TencentCloud/tencentcloud-cli/archive/3.0.1284.1.tar.gz"
+  sha256 "010f81ae012e7b996e4485a0afd4cc9e175c2ac7b2a049c4af2ce192b4349d09"
+  license "Apache-2.0"
+
+  depends_on "python@3.9"
+
+  def install
+    venv = virtualenv_create(libexec, "python3", without_pip: false)
+    system libexec/"bin/pip", "install", "-v", 
+                              "--ignore-installed", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "tccli"
+    venv.pip_install_and_link buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "pyinstaller"
+
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tencentcloud-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
