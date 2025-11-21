9a14d8222b5de35c16f094274be96df4efce3c87
tencentcloudapi <tencentcloudapi@tencent.com>
2025-11-21 12:34:10 +0800

    release 3.1.1.1

--- tccli.rb
+++ tccli.rb
@@ -5,8 +5,8 @@ class Tccli < Formula
   include Language::Python::Virtualenv
   desc "Tencent Cloud API 3.0 Command Line Interface"
   homepage "https://cloud.tencent.com/document/product/440/6176"
-  url "https://github.com/TencentCloud/tencentcloud-cli/archive/3.0.1421.1.tar.gz"
-  sha256 "273ef16a4e7471d7b936683fbb6a7bd0b137550090722656f0ba8335224558a2"
+  url "https://github.com/TencentCloud/tencentcloud-cli/archive/3.1.1.1.tar.gz"
+  sha256 "1287328b0312f210201ca080ffeb5883b943df55728142def63ba666a80f8197"
   license "Apache-2.0"
 
   depends_on "python@3.10"
